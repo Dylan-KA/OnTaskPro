43c7dda8dcc0a048ffddb28edd1b01931def2f31
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-09-03 15:09:04 +1000

    Initial Commit

---
+++ OnTaskPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnTaskPro
+//
+//  Created by Dylan Archer on 3/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnTaskPro/OnTaskProApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTaskProApp.swift
+//  OnTaskPro
+//
+//  Created by Dylan Archer on 3/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnTaskProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
